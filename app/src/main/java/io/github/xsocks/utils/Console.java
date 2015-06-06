25ad4446d89234f6b18f54dfbc579612000311c8
unknown <ken.i18n@gmail.com>
2015-06-06 11:36:50 +0800

    remove libsuperuser

--- app/src/main/java/io/github/xsocks/utils/Console.java
+++ app/src/main/java/io/github/xsocks/utils/Console.java
@@ -1,43 +1,10 @@
 package io.github.xsocks.utils;
 
-import java.util.ArrayList;
-
-import eu.chainfire.libsuperuser.Shell;
-
 public class Console {
 
-    private static Shell.Interactive openShell() {
-        Shell.Builder builder = new Shell.Builder();
-        return builder.useSH().setWatchdogTimeout(10).open();
-    }
-
-    public static void runCommand(String command) {
-        ArrayList<String> sb = new ArrayList<>();
-        sb.add(command);
-        String[] commands = new String[sb.size()];
-        sb.toArray(commands);
-        runCommand(commands);
-    }
-
-    public static String runCommand(String[] commands) {
-        Shell.Interactive shell = openShell();
-        StringBuilder sb = new StringBuilder();
-        shell.addCommand(commands, 0, (commandCode, exitCode, output) -> {
-            if (exitCode < 0) {
-                shell.close();
-            } else {
-                for (String line : output) {
-                    sb.append(line).append('\n');
-                }
-            }
-        });
-        if (shell.waitForIdle()) {
-            shell.close();
-            return sb.toString();
-        }
-        else {
-            shell.close();
-            return null;
+    public static void runCommand(String[] cmds) {
+        for (String cmd : cmds) {
+            io.github.xsocks.System.exec(cmd);
         }
     }
 }
